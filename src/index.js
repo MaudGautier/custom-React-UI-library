3363bb970acdda5cfa1d57d52ac23491a54c5518
Maud Gautier <maud.j.gautier@gmail.com>
2023-03-06 15:09:18 +0100

    feat: add v0 for html page

---
+++ src/index.js
@@ -0,0 +1,12 @@
+function consoleLogToto () {
+  console.log("toto")
+}
+
+
+TODO_LIST = []
+
+function addItemToTodoList (item) {
+  console.log("TODO LIST without item", TODO_LIST)
+  TODO_LIST.push(item)
+  console.log("TODO LIST with item", TODO_LIST)
+}
