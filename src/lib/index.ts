bd23e2280880f77f81210a6beb0632992703abb1
Maud Gautier <maud.j.gautier@gmail.com>
2023-06-20 14:11:50 -0600

    chore: remove dead code

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -11,24 +11,6 @@ export type VirtualDomElement = {
   // id: string;
 };
 
-export function interpret(virtualDomElement: VirtualDomElement): Element {
-  const element = document.createElement(virtualDomElement.tag);
-  element.className = virtualDomElement.className;
-  if (virtualDomElement.onClick) {
-    element.onclick = virtualDomElement.onClick;
-  }
-
-  if (typeof virtualDomElement.children === "string") {
-    element.textContent = virtualDomElement.children;
-
-    return element;
-  }
-
-  virtualDomElement.children.map(interpret).forEach((child) => element.append(child));
-
-  return element;
-}
-
 let state = {};
 
 export function useState<State>(initialValue, slug): [getState: () => State, updateState: (t: State) => void] {
